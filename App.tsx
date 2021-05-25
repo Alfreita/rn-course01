37f343d90a61e740f0e4c75e1743e45c44e31a23
Victor Almeida de Freitas <victor.freitas08@hotmail.com>
2021-05-25 17:56:16 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <StatusBar style="auto" /> */}
+      <View style={styles.header}>
+        <TextInput placeholder="course goals" style={styles.textInput} />
+        <Button title="add" onPress={() => {}} />
+      </View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  textInput: {
+    padding: 10,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    width: "80%",
   },
 });
