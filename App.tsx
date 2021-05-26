421e3f6ce3baebe8e8ef3fa7f80a544ae62836ea
Victor Almeida de Freitas <victor.freitas08@hotmail.com>
2021-05-26 16:55:36 -0300

    managing the current state of a array of state

--- App.tsx
+++ App.tsx
@@ -1,14 +1,29 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [enteredGoalS, setEnteredGoalS] = useState<any>([]);
+
+  const goalInputHandler = (enteredText: string) => {
+    setEnteredGoal(enteredText);
+  };
+  const addGoalHandler = () => {
+    setEnteredGoalS((currentGoals: any) => [...currentGoals, enteredGoal]);
+  };
+
   return (
     <View style={styles.container}>
       {/* <StatusBar style="auto" /> */}
       <View style={styles.header}>
-        <TextInput placeholder="course goals" style={styles.textInput} />
-        <Button title="add" onPress={() => {}} />
+        <TextInput
+          placeholder="course goals"
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="add" onPress={addGoalHandler} />
       </View>
       <View></View>
     </View>
